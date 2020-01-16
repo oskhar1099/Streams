18d8350288fe9f9fee933d717a72a1ae511c8261
Oskhar Arrieta <oskhara@uninorte.edu.co>
2020-01-16 12:13:31 -0500

    Added OAuth

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
-
+import Header from './Header';
 
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
+                    <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
                     <Route path="/streams/edit" exact component={StreamEdit} />
---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component{
+    state = {isSignedIn: null};
+
+    componentDidMount(){
+        window.gapi.load('client:auth2', () => { 
+            window.gapi.client.init({
+                clientId: '109828268130-k0nbh2o9pv672fm8semeocvtjhu876eq.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get()})
+            });        
+        });
+    }
+
+    renderAuthButton(){
+        if (this.state.isSignedIn === null){
+            return <div>No idea</div>;
+        }else if (this.state.isSignedIn) {
+            return <div>Im signed in</div>;
+        }else{
+            return <div>Im not signed in</div>;
+        }
+    }
+
+    render(){
+        return <div>{this.renderAuthButton()}</div>
+    }
+}
+
+export default GoogleAuth;
